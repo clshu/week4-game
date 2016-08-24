294e00ada0d942d15cb1d73425f48135786ebe20
clshu <ccss0630@yahoo.com>
2016-08-24 02:29:29 -0500

    Add wallpaper and images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,26 +5,30 @@ var characterArray = [];
 var enemyArray = [];
 var mainCharacter = null;
 var defender = null;
-var gameMessage = '';
+var youWin = 0;
+var youLose = 1;
 
 // Constants
-var line1 = '<h2>Your Character</h2>';
-var line2 = '<h2>Enemies Available To Attack</h2>';
-var line3 = '<h2>Flight Section</h2>';
+var line1 = '<h2>Main Character</h2>';
+var line2 = '<h2>Enemies Available To Fight</h2>';
+var line3 = '<h2>Fight Section</h2>';
 var line4 = '<h2>Defender</h2>';
 var characterClasses = 'demoBox bg-white bd-visible bd-green character';
 var enemyClasses = 'demoBox bg-red bd-visible bd-black enemy';
 var defenderClasses = 'demoBox bg-black bd-visible bd-green text-white'; // don't handle events
 var emptyBoxClasses = 'demoBox emptyBox';
-var attackId = 'attack';
-var restartId = 'restart';
+var imgClass = 'img-holder';
+var imgPath = 'assets/images/';
+var attackId = 'Attack';
+var restartId = 'Restart';
 var noEnemyMsg = 'No Enemy to fight!';
-var winMsg = '<br>You Won!! Game Over';
-var loseMsg = '<br>You Lost!! Game Over';
+
 
 // Object
-function Character(name, bAP, cAP, hP) {
+function Character(name, movieName, img, bAP, cAP, hP) {
 	this.name = name;
+	this.movieName = movieName;
+	this.img = img;
 	this.baseAttackPower = bAP;
 	this.attackPower = bAP; // same as baseAttackPower
 	this.counterAttackPower = cAP;
@@ -34,19 +38,27 @@ function Character(name, bAP, cAP, hP) {
 // Functions
 function loadData() {
 	var array = [];
-	var ch1 = new Character('Player 0',
+	var ch1 = new Character('Clint Eastwood',
+							'Blondie',
+							'blondie.jpg',
 							8,
 							15,
 							120);
-	var ch2 = new Character('Player 1',
+	var ch2 = new Character('Eli Wallach',
+							'Tuco',
+							'tuco.jpg',
 							10,
 							5,
 							100);
-	var ch3 = new Character('Player 2',
+	var ch3 = new Character('Lee Van Cleef',
+							'Angel',
+							'angel.jpg',
 							20,
 							20,
 							150);
-	var ch4 = new Character('Player 3',
+	var ch4 = new Character('Mario Brega',
+							'Cpl. Wallace',
+							'wallace.jpg',
 							15,
 							10,
 							180);
@@ -72,16 +84,25 @@ function setMessage(msg) {
 	$('#message').html(msg);
 }
 function createStatMessage(attacker, defender) {
-	var msg = 'You attack ' + defender.name + ' for ' + attacker.attackPower + ' damages';
-	msg += '<br>' + defender.name + ' attack you back for ' + defender.counterAttackPower + ' damages';
+	var msg = attacker.movieName + ' attacks ' + defender.movieName + ' for <span class="statNumber">' + attacker.attackPower + '</span> damages';
+	msg += '<br>' + defender.movieName + ' attacks ' + attacker.movieName + ' back for <span class="statNumber">' + defender.counterAttackPower + '</span> damages';
 	return msg;	
 }
-function createDefeatMessage(obj) {
-	var msg = 'You defeated ' + obj.name + ' !';
+function createDefeatMessage(obj1, obj2) {
+	var msg = obj1.movieName + ' defeated ' + obj2.movieName + ' !';
 	return msg;
 }
-function createBothLoseMessage(obj) {
-	var msg = 'You and ' + obj.name + ' both lost!! Game Over'
+function createBothLoseMessage(obj1, obj2) {
+	var msg = obj1.movieName + ' and ' + obj2.movieName + ' both lost!! Game Over'
+	return msg;
+}
+function createWinLoseMessage(flag, obj) {
+	var msg;
+	if (flag == youWin) {
+		msg = '<br>' + obj.movieName + ' Won!! Game Over';
+	} else if (flag == youLose) {
+		msg = '<br>' + obj.movieName + ' Lost!! Game Over';
+	}
 	return msg;
 }
 function createRow(cls, id) {
@@ -89,11 +110,13 @@ function createRow(cls, id) {
 	return row;
 }
 function createBox(divClasses, index, obj) {
-	var img = null;
+	var img, fullPath;
 	var box = $('<div>').addClass(divClasses).attr('value', index);
 	if (obj != null) {
-		box.append('<h4 id="name">' + obj.name + '</h4>');
-		img = $('<div>').addClass('img-holder');
+		box.append('<h4 id="title">' + obj.movieName + '</h4>');
+		img = $('<div>').addClass(imgClass).attr('name', obj.name).attr('movie-name', obj.movieName);
+		fullPath = imgPath + obj.img;
+		img.css('background-image', 'url("' + fullPath + '")');
 		box.append(img);
 		box.append('<h4 id="HP">' + obj.healthPoint + '</h4>');
 	}
@@ -114,7 +137,7 @@ function appendBoxes(rowObj, divClasses, classArray) {
 
 }
 function createButton(buttonId) {
-	return ($('<button>').attr('id', buttonId).text(buttonId));
+	return ($('<button>').attr('id', buttonId.toLowerCase()).text(buttonId));
 }
 
 function createFirstPage() {
@@ -223,7 +246,7 @@ $(document).ready(function () {
 			if (mainCharacter.healthPoint <= 0 && defender.healthPoint <= 0) {
 				// both lose
 				gameOver = true;
-				msg = createBothLoseMessage(defender);
+				msg = createBothLoseMessage(mainCharacter, defender);
 				setMessage(msg);
 				// remove defender
 				defender = null;
@@ -232,20 +255,20 @@ $(document).ready(function () {
 			} else if (mainCharacter.healthPoint <= 0) {
 				// mainCharacter lose
 				gameOver = true;
-				setMessage(loseMsg);
+				setMessage(createWinLoseMessage(youLose, mainCharacter));
 			} else if (defender.healthPoint <= 0) { 
 				// defender lose
 				// append an emptyBox to row3
 				$('#row3').empty();
 				var boxObj = createBox(emptyBoxClasses, 0, null);
 				$('#row3').append(boxObj);
-				msg = createDefeatMessage(defender);
+				msg = createDefeatMessage(mainCharacter, defender);
 				// remove defender
 				defender = null;
 
 				if (enemyArray.length == 0) {
 					gameOver = true;
-					msg += winMsg;
+					msg += createWinLoseMessage(youWin, mainCharacter);
 				}
 
 				setMessage(msg);
